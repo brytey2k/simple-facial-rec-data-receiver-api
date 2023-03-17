1e8cc73c755fb579a3dbd6e1904de24c038d49cb
Bright Nkrumah <brytey2k@gmail.com>
2023-03-17 22:38:28 +0000

    Bug fix: corrects environment variable for DB_PASSWORD used in code

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 // Configure bodyParser to parse JSON requests
 app.use(bodyParser.json());
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
 });
